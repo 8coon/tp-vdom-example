5c07d61b889fa7a0488220df56cbb352e144c344
Kaibito <gpuzzletime@icloud.com>
2019-11-09 18:43:42 +0300

    master: add update

--- vdom.js
+++ vdom.js
@@ -21,12 +21,78 @@ function create(vnode) {
 		}
 	}
 
+	// Связываем виртуальный и реальный DOM
 	node._vnode = vnode;
+	// Переносим ключ для оптимизации сравнения при обновлении родителя
+	node._vnodeKey = vnode.key;
+
 	return node;
 }
 
-function update() {
+function updateAttrs(node, prevAttrs, attrs) {
+	// Задаём новые значения атрибутов
+	if (attrs) {
+		for (const [name, value] of Object.entries(attrs)) {
+			node.setAttribute(name, value);
+		}
+	}
 
+	// Удаляем старые атрибуты, которых не встретилось в новых
+	if (prevAttrs) {
+		for (const name of Object.keys(prevAttrs)) {
+			if (!attrs || !attrs.hasOwnProperty(name)) {
+				node.removeAttribute(name);
+			}
+		}
+	}
+}
+
+function updateChildren(node, prevChildren, children) {
+	const childLength = children ? children.length : 0;
+
+	for (let i = 0; i < childLength; i++) {
+		const prevVchild = prevChildren && prevChildren[i];
+		const vchild = children[i];
+
+		const fromString = typeof prevVchild === 'string';
+		const toString = typeof vchild === 'string';
+
+		if (fromString && toString) {
+			// Если старый и новый элемент -- строки
+			// Внимание! .childNodes, не .children!
+			node.childNodes[i].textContent = vchild;
+		} else if (
+			!fromString &&
+			!toString &&
+			prevVchild &&
+			prevVchild.key === vchild.key
+		) {
+			// Ключи совпадают -- можем обновить оптимально
+			update(node.childNodes[i], vchild);
+		} else {
+			// Если строка превращается в элемент или наоборот
+			// Либо у элементов не совпали ключи
+			// Вставляем новую ноду перед текущей, старая нода при этом "всптывает" в конец
+			node.insertBefore(create(vchild), node.childNodes[i]);
+		}
+	}
+
+	const curChildLength = node.childNodes.length;
+
+	// Удаляем "лишние" узлы
+	for (let i = childLength; i < curChildLength; i++) {
+		node.removeChild(node.childNodes[childLength]);
+	}
+}
+
+function update(node, vnode) {
+	updateAttrs(node, node._vnode.attrs, vnode.attrs);
+	updateChildren(node, node._vnode.children, vnode.children);
+
+	// Обновлем связь с virtual DOM
+	node._vnode = vnode;
+
+	return node;
 }
 
 module.exports = {
--- vdom.tests.js
+++ vdom.tests.js
@@ -8,35 +8,248 @@ describe('vdom', () => {
 		});
 
 		test('Создаёт один элемент', () => {
-			expect(vdom.create(
-				{
-					tag: 'DIV',
-				}
-			)).toMatchSnapshot();
+			expect(vdom.create({tag: 'DIV'})).toMatchSnapshot();
 		});
 
 		test('Создаёт один элемент с двумя атрибутами', () => {
-			expect(vdom.create(
-				{
+			expect(vdom.create({
+				tag: 'DIV',
+				attrs: {
+					style: 'background-color: red;',
+					'tab-index': 0
+				}
+			})).toMatchSnapshot();
+		});
+
+		test('Создаёт один элемент с двумя детьми', () => {
+			expect(vdom.create({
+				tag: 'DIV',
+				children: [
+					{tag: 'SPAN'},
+					'Some text'
+				]
+			})).toMatchSnapshot();
+		});
+	});
+
+	describe('update', () => {
+		describe('attrs', () => {
+			test('Обновляет атрибуты 2 -> 2', () => {
+				const node = vdom.create({
 					tag: 'DIV',
 					attrs: {
 						style: 'background-color: red;',
 						'tab-index': 0
 					}
-				}
-			)).toMatchSnapshot();
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					attrs: {
+						style: 'background-color: black;',
+						'tab-index': 2
+					}
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет атрибуты 2 -> 1', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					attrs: {
+						style: 'background-color: red;',
+						'tab-index': 0
+					}
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					attrs: {
+						style: 'background-color: black;'
+					}
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет атрибуты 1 -> 2', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					attrs: {
+						'tab-index': 0
+					}
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					attrs: {
+						style: 'background-color: black;',
+						'tab-index': 2
+					}
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет атрибуты 2 -> 0', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					attrs: {
+						style: 'background-color: red;',
+						'tab-index': 0
+					}
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет атрибуты 0 -> 2', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					attrs: {
+						style: 'background-color: black;',
+						'tab-index': 2
+					}
+				})).toMatchSnapshot();
+			});
 		});
 
-		test('Создаёт один элемент с двумя детьми', () => {
-			expect(vdom.create(
-				{
+		describe('children', () => {
+			test('Обновляет детей 1 -> 1', () => {
+				const node = vdom.create({
 					tag: 'DIV',
 					children: [
-						{tag: 'SPAN'},
+						{tag: 'SPAN', key: 'A'},
+					]
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						{tag: 'SPAN', key: 'A'},
+					]
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 1 -> 1 разные типы', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					children: [
+						{tag: 'SPAN', key: 'A'},
+					]
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						'Some text',
+					]
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 1 -> 1 разные ключи', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					children: [
+						{tag: 'SPAN', key: 'A'},
+					]
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						{tag: 'B', key: 'B'},
+					]
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 2 -> 2', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					children: [
+						{tag: 'SPAN', key: 'A'},
 						'Some text'
 					]
-				}
-			)).toMatchSnapshot();
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						{tag: 'SPAN', attrs: {disabled: 'disabled'}, key: 'A'},
+						'Some text updated'
+					]
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 2 -> 2 разные типы', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					children: [
+						{tag: 'SPAN', key: 'A'},
+						'Some text'
+					]
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						'Some text updated',
+						{tag: 'SPAN', attrs: {disabled: 'disabled'}, key: 'A'},
+					]
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 2 -> 2 разные ключи', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					children: [
+						{tag: 'B', attrs: {A: 'A'}, key: 'A'},
+						{tag: 'I', attrs: {B: 'B'}, key: 'B'},
+					]
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						{tag: 'B', attrs: {B: 'B'}, key: 'B'},
+						{tag: 'I', attrs: {A: 'A'}, key: 'A'},
+					]
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 2 -> 0', () => {
+				const node = vdom.create({
+					tag: 'DIV',
+					children: [
+						{tag: 'B', attrs: {A: 'A'}, key: 'A'},
+						{tag: 'I', attrs: {B: 'B'}, key: 'B'},
+					]
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV'
+				})).toMatchSnapshot();
+			});
+
+			test('Обновляет детей 0 -> 2', () => {
+				const node = vdom.create({
+					tag: 'DIV'
+				});
+
+				expect(vdom.update(node, {
+					tag: 'DIV',
+					children: [
+						{tag: 'B', attrs: {A: 'A'}, key: 'A'},
+						{tag: 'I', attrs: {B: 'B'}, key: 'B'},
+					]
+				})).toMatchSnapshot();
+			});
+		});
+
+		test('Обновляет один элемент', () => {
+			const node = vdom.create({tag: 'DIV'});
+			expect(vdom.update(node, {tag: 'DIV'})).toMatchSnapshot();
 		});
 	});
 
